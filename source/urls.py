49a8a64d2cdf89766a45d3761a10ae43c93b07ce
danchello <danchello.man@gmail.com>
2023-01-05 15:34:04 +0600

    Поправил docker-compose.yml еще раз

--- source/urls.py
+++ source/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from source.views import check_status_view
 
 urlpatterns = [
-    path('', check_status_view, name='check_status')
+    path('check/', check_status_view, name='check_status')
 ]
